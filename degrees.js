130692bfc26a5e771026897893be0fc902fd7ce4
Łukasz Bułdak <posterwad@gmail.com>
2026-03-23 09:50:58 +0100

    First code in js

---
+++ degrees.js
@@ -0,0 +1,11 @@
+//Code that converts Kelvin to Celsius, and then to Fahrenheit.
+//kelvin
+const kelvin = 293;
+//celsius
+const celsius = kelvin - 273;
+//fahrenheit
+const fahrenheit = Math.floor(celsius(9/5)+32);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+//newton
+const newton = Math.floor(celsius(33/100));
+console.log(newton);
