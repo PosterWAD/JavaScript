e19340ee77078e6a384bfde81344cedd6aef025b
Łukasz Bułdak <posterwad@gmail.com>
2026-03-23 10:36:46 +0100

    Second Project

---
+++ dogYears.js
@@ -0,0 +1,11 @@
+let myAge = 22;
+let earlyYears = 2;
+earlyYears *= 10.5;
+console.log(earlyYears);
+console.log(myAge);
+let laterYears = myAge;
+laterYears *= 4;
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Luke'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`);
